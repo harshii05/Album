cb66db4208a69bc3083a55b730680d9b341cc240
harsh <harshitagunwani29@gmail.com>
2022-11-13 03:38:14 +0530

    second commit

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 
 import express from 'express';
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
@@ -9,6 +10,8 @@ import userRouter from "./routes/user.js";
 
 const app = express();
 
+dotenv.config();
+
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
@@ -16,10 +19,15 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL = 'mongodb+srv://memories:harshita29@cluster0.czuzghu.mongodb.net/?retryWrites=true&w=majority';
-const PORT = process.env.PORT|| 5000;
+app.get('/',(req,res)=>{
+  res.send('APP IS RUNNING');
+});
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+//const CONNECTION_URL = 'mongodb+srv://memories:harshita29@cluster0.czuzghu.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT|| 5000;
+const uri=process.env.CONNECTION_URL;
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
